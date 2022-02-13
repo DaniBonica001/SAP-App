89ab4b7099d26641ef9f2ef192acf80a97c703d6
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2022-02-13 14:21:28 -0500

    Object Error404 was added.

--- app.js
+++ app.js
@@ -101,6 +101,20 @@ let About = {
         
 }
 
+let Error404 = {
+
+    render : async () => {
+        let view =  /*html*/`
+            <section class="section">
+                <h1> 404 Error </h1>
+            </section>
+        `
+        return view
+    }
+    , after_render: async () => {
+    }
+}
+
 const routes = {
     '/'             : Home
     , '/about'      : About
