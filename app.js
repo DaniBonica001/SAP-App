3d4a8dd7bb116fc71db330b3c58973098e7fc7e5
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2022-02-12 20:47:50 -0500

    Object Bottombar was added.

--- app.js
+++ app.js
@@ -22,6 +22,23 @@ const Utils = {
     }
 }
 
+let Bottombar = {
+    render: async () => {
+        let view =  /*html*/`
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    This is my foot. There are many like it, but this one is mine.
+                </p>
+            </div>
+        </footer>
+        `
+        return view
+    },
+    after_render: async () => { }
+
+}
+
 const routes = {
     '/'             : Home
     , '/about'      : About
