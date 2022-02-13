146c789d116f7ff5a37769c4e912bcc0a1d5af5f
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-02-13 16:38:40 -0500

    Added home

--- app.js
+++ app.js
@@ -132,6 +132,27 @@ let getPostsList = async () => {
    }
 }
 
+let Home = {
+    render : async () => {
+        let posts = await getPostsList()
+        let view =  /*html*/`
+            <section class="section">
+                <h1> Home </h1>
+                <ul>
+                    ${ posts.map(post => 
+                        /*html*/`<li><a href="#/p/${post.id}">${post.title}</a></li>`
+                        ).join('\n ')
+                    }
+                </ul>
+            </section>
+        `
+        return view
+    }
+    , after_render: async () => {
+    }
+ 
+ }
+
 const routes = {
     '/'             : Home
     , '/about'      : About
