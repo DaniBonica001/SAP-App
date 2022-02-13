6bf4bc4c3362deac1e08f691d459a1a5057acbc0
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2022-02-13 15:17:09 -0500

    Object getPostsList was added.

--- app.js
+++ app.js
@@ -115,6 +115,23 @@ let Error404 = {
     }
 }
 
+let getPostsList = async () => {
+    const options = {
+       method: 'GET',
+       headers: {
+           'Content-Type': 'application/json'
+       }
+   };
+   try {
+       const response = await fetch(`https://5bb634f6695f8d001496c082.mockapi.io/api/posts`, options)
+       const json = await response.json();
+       // console.log(json)
+       return json
+   } catch (err) {
+       console.log('Error getting documents', err)
+   }
+}
+
 const routes = {
     '/'             : Home
     , '/about'      : About
